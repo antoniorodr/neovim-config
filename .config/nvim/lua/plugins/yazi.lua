937efea4f572a79d0b94498d7a01dd18676d8d98
Antonio Rodriguez <antonioinorge@hotmail.com>
2025-03-06 15:35:18 +0100

    fix yazi plugin

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -1,32 +1,7 @@
 return {
   "mikavilpas/yazi.nvim",
   event = "VeryLazy",
-  dependencies = { "folke/snacks.nvim", lazy = true },
   keys = {
-    {
-      "<leader>_",
-      mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
-      desc = "Open Yazi at current file",
-    },
-    {
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open Yazi in working directory",
-    },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume last Yazi session",
-    },
+    { "<leader>_", "<cmd>Yazi<CR>", desc = "Yazi" },
   },
-  opts = {
-    open_for_directories = false,
-    keymaps = {
-      show_help = "<f1>",
-    },
-  },
-  init = function()
-    vim.g.loaded_netrwPlugin = 1
-  end,
 }
